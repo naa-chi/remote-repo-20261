ede505160847f12ec671a91d911852c1b6ee90aa
naa-chi <dxsnnr@gmail.com>
2026-06-15 17:10:37 -0400

    fixed everything HOTFIX

---
+++ auth/src/main/java/com/gTransitProject/auth/exception/resourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gTransitProject.auth.exception;
+
+public class resourceNotFoundException extends RuntimeException {
+    public resourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ driver/src/main/java/com/gTransitProject/driver/exception/resourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gTransitProject.driver.exception;
+
+public class resourceNotFoundException extends RuntimeException {
+    public resourceNotFoundException(String message) {
+        super(message);
+    }
+}
